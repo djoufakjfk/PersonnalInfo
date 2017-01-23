f3cd2473f63ed10a0b58be7a883781af096243a6
Jean Francois Djoufak Kengue <jeankengue@Jeans-MacBook-Air.local>
2017-01-23 15:22:19 -0500

    Initial Commit

---
+++ PersonnalInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonnalInfo
+//
+//  Created by Jean Francois Djoufak Kengue on 2017-01-23.
+//  Copyright © 2017 Djoufak Corporate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
